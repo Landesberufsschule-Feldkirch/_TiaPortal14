a332c5dfb23d074f9d93ff417bd2fbac2cd7ecf5
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2021-03-11 14:09:42 +0100

    Änderungen vom Labor

--- TEST_SCL_Labor_Getriebemotor/start.cmd
+++ TEST_SCL_Labor_Getriebemotor/start.cmd
@@ -1,3 +1,5 @@
 h:
 cd h:\TiaPortal14\Getriebemotor
 start Getriebemotor.ap14
+cd net5.0-windows
+start LaborGetriebemotor.exe
--- TEST_SCL_Labor_Linearachse/start.cmd
+++ TEST_SCL_Labor_Linearachse/start.cmd
@@ -1,3 +1,5 @@
 h:
 cd h:\TiaPortal14\Linearachse
 start Linearachse.ap14
+cd net5.0-windows
+start LaborLinearachse
