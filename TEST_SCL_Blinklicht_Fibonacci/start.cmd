4b847040ebff41bcba64a766bbbb4760b5845fe6
Kurt Linder <kurt.linder@lbsfe1.snv.at>
2021-05-13 17:37:22 +0200

    Änderungen vom Labor

---
+++ TEST_SCL_Blinklicht_Fibonacci/start.cmd
@@ -0,0 +1,5 @@
+h:
+cd h:\TiaPortal14\Blinklicht_Fibonacci
+start Blinklicht_Fibonacci.ap14
+cd net5.0-windows
+start Blinklicht_Fibonacci.exe
---
+++ TEST_SCL_WordClock/start.cmd
@@ -0,0 +1,5 @@
+h:
+cd h:\TiaPortal14\WordClock
+start WordClock.ap14
+cd net5.0-windows
+start WordClock.exe
